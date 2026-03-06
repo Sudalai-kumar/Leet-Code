d3926641cf73ba1c78c0322bd42144aa6baf3990
M Sudalai Kumar <150756741+Sudalai-kumar@users.noreply.github.com>
2026-03-06 22:29:22 +0530

    Time: 20 ms (77.99%) | Memory: 53.8 MB (80.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,13 +1,20 @@
 class Solution {
+    // private String res="";
+    // private void LCS_print(char[][] b,String s2,int i,int j){
+    //     if (b[i][j]=='d'){
+    //         res=s2.charAt(j-1)+res;
+    //         LCS_print(b,s2,i-1,j-1);
+    //     }
+    //     else if(b[i][j]=='u'){
+    //         LCS_print(b,s2,i-1,j);
+    //     }
+    //     else if(b[i][j]=='l'){
+    //         LCS_print(b,s2,i,j-1);
+    //     }
+    // }
     private int LCS_length(String text1, String text2,int m,int n){
         int c[][]=new int [m+1][n+1];
-        // char b[][]=new char [m][n];
-        for(int i=0;i<=m;i++){
-            c[i][0]=0;
-        }
-        for(int j=0;j<=n;j++){
-            c[0][j]=0;
-        }
+        // char b[][]=new char [m+1][n+1];
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
@@ -24,6 +31,20 @@ class Solution {
                 }
             }
         }
+        // for(int i=0;i<=m;i++){
+        //     for(int j=0;j<=n;j++){
+        //         System.out.print(" "+c[i][j]+""+b[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+        // int LCS_len=c[m][n];
+        // for(int j=n;j>=0;j--){
+        //     if(c[m][j]==LCS_len){
+        //         LCS_print(b,text2,m,j);
+        //         System.out.println(res);
+        //         res="";
+        //     }
+        // }
         return c[m][n];
     }
     public int longestCommonSubsequence(String text1, String text2) {
