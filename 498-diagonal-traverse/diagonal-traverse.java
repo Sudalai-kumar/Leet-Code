1f06690c037f9abfc0acbb173e330613b1fd63cc
M Sudalai Kumar <150756741+Sudalai-kumar@users.noreply.github.com>
2026-03-30 23:42:28 +0530

    Time: 2 ms (98.58%) | Memory: 48.2 MB (41.76%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m=mat.length;
+        int n=mat[0].length;
+        int res[]=new int[m*n];
+        int row=0,col=0;
+        for(int i=0;i<m*n;i++){
+            res[i]=mat[row][col];
+            if((row+col)%2 == 0){
+                if(col==n-1) row++;
+                else if(row==0) col++;
+                else{ col++;row--;}
+            }else{
+                if(row==m-1) col ++;
+                else if(col==0) row++;
+                else {row++;col--;}
+            }
+        }
+        return res;
+    }
+}
